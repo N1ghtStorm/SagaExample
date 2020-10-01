35a0de59ed2b5c7754d45de846835285e7b9457b
N1ghtStorm <bushuevdv@mail.ru>
2020-10-01 11:36:11 +0300

    add saga example

--- HotDogsOperations/HotDogsOperations/Sagas/HotDogStateMachine.cs
+++ HotDogsOperations/HotDogsOperations/Sagas/HotDogStateMachine.cs
@@ -11,9 +11,9 @@ namespace HotDogsOperations.Sagas
     {
 
         public Event<ICreateSausage> SubmitCreateSausage { get; private set; }
-        public Event<ICreateSausage> SausageCreataApproved { get; private set; }
-        public Event<ICreateSausage> SubmitCreateBun { get; private set; }
-        public Event<ICreateSausage> BunCreateApproved { get; private set; }
+        public Event<ISausageCreated> SausageCreataApproved { get; private set; }
+        public Event<ICreateBun> SubmitCreateBun { get; private set; }
+        public Event<IBunCreated> BunCreateApproved { get; private set; }
 
         public State SausageSubmitted { get; set; }
         public State SausageCreated { get; set; }
@@ -25,12 +25,23 @@ namespace HotDogsOperations.Sagas
         {
             //InstanceState(x => x.CurrentState);
             Event(() => SubmitCreateSausage, x => x.CorrelateById(context => context.Message.CorrelationId));
+            Event(() => SausageCreataApproved, x => x.CorrelateById(context => context.Message.CorrelationId));
+            Event(() => SubmitCreateBun, x => x.CorrelateById(context => context.Message.CorrelationId));
+            Event(() => BunCreateApproved, x => x.CorrelateById(context => context.Message.CorrelationId));
 
             InstanceState(x => x.CurrentState);
 
             Initially(
                 When(SubmitCreateSausage)
                 .TransitionTo(SausageSubmitted));
+
+            During(SausageSubmitted,
+                When(SausageCreataApproved).TransitionTo(SausageCreated));
+
+            During(SausageCreated,
+                    When(SubmitCreateBun).TransitionTo(BunSubmitted));
+
+            During(BunSubmitted, When(BunCreateApproved).TransitionTo(BunCreated).Finalize());
 
             
             //ICreateSausage
